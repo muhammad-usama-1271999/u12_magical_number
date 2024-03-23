70819393e78c67632b2e2b7a1bc4dc1010b4148c
muhammad-usama-1271999 <160097159+muhammad-usama-1271999@users.noreply.github.com>
2024-03-23 11:16:02 +0500

    number guess game

---
+++ p.js
@@ -0,0 +1,19 @@
+#!  /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let answer = await inquirer.prompt([{
+        name: "magicNumber",
+        type: "number",
+        message: "find the magic number (0 - 9)",
+    }]);
+let num = Math.floor(Math.random() * 6) + 1;
+console.log(num);
+if (answer.magicNumber === num) {
+    console.log(chalk.blueBright("You win , You find the correct number "));
+}
+else if (answer.magicNumber === (num + 1) || answer.magicNumber === (num - 1)) {
+    console.log(chalk.yellowBright("You are very close to magic number"));
+}
+else {
+    console.log(chalk.red("You lose this game pleas try again"));
+}
